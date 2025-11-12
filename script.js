ff07a465eb7c6d86e19adfeb81857352c4f29150
Erick <erick.vlozano@gmail.com>
2025-11-12 22:14:23 +0100

    Basic JS structure

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
+class Paddle {
+    constructor() {
+        this.x = 350; this.y = 520; this.w = 100; this.h = 16; this.v = 8;
+    }
+}
+class Ball {}
+class Brick {}
+class PowerUp {}
+class LevelGen {}
+class PlayerStats {}
 
+startNewRun(seed);
+nextLevel();
+applyUpgrade(upgradeType);
+saveMetaProgress();
+loadMetaProgress();
+
+function initGame() {
+
+}
+
+function update() {
+
+}
+
+function draw() {
+
+}
+
+function loop() {
+
+}
